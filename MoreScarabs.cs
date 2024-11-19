b347664d0a2a61169e1285e1ff1a6cc3e9d47749
binbinmods <binbinmods@gmail.com>
2024-11-19 18:00:58 -0500

    Initial Commit - Untested

---
+++ MoreScarabs.cs
@@ -0,0 +1,186 @@
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using static Obeliskial_Essentials.Essentials;
+using System;
+using static MoreScarabs.CustomFunctions;
+using System.Text.RegularExpressions;
+
+// Make sure your namespace is the same everywhere
+namespace MoreScarabs
+{
+
+    [HarmonyPatch] //DO NOT REMOVE/CHANGE
+
+    public class MoreScarabs
+    {
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(MatchManager), "CreateNPC")]
+        public static void CreateNPCReversePatch(NPCData _npcData,
+            string effectTarget = "",
+            int _position = -1,
+            bool generateFromReload = false,
+            string internalId = "",
+            CardData _cardActive = null) 
+        {
+            //This is intentionally a stub
+            PLog("Executing CreateNPCReversePatch");
+        }
+
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MatchManager), nameof(MatchManager.EndTurn))]
+        public static void EndTurnPrefix(MatchManager __instance)
+        {
+            /* 
+            This is going to show how to reference a private variable
+            */
+            Plugin.Log.LogInfo("End Turn Prefix for Jades - START");
+
+            MatchManager matchManager = __instance;
+
+            if (matchManager == null || matchManager.MatchIsOver)
+                return;
+
+            string scarabSpawned = Traverse.Create(matchManager).Field("scarabSpawned").GetValue<string>();
+            CombatData combatData = Traverse.Create(matchManager).Field("combatData").GetValue<CombatData>();
+            int currentRound = Traverse.Create(matchManager).Field("currentRound").GetValue<int>();
+            NPC[] TeamNPC = Traverse.Create(matchManager).Field("TeamNPC").GetValue<NPC[]>();
+
+            if (currentRound == 0 || scarabSpawned == "" || (UnityEngine.Object)combatData != (UnityEngine.Object)null || (UnityEngine.Object)Globals.Instance.GetNodeData(AtOManager.Instance.currentMapNode) != (UnityEngine.Object)null)
+                return;
+
+            int spawnChance = Plugin.GuaranteedSpawn.Value ? 100 : Plugin.PercentChanceToSpawn.Value;
+
+            int scarabType = Plugin.OnlySpawnJades.Value ? 2 : matchManager.GetRandomIntRange(0, 4);
+            
+            // if scarabType==2 then Jade, if 1 then Gold, if 0 then Crystal. Else scourge
+            scarabSpawned = scarabType == 2 ? "jadescarab" : scarabType == 1 ? "goldenscarab" : scarabType == 0 ? "crystalscarab" : "scourgescarab";
+
+
+
+            bool isValidCombat = false;
+            switch (Globals.Instance.GetNodeData(AtOManager.Instance.currentMapNode).NodeCombatTier)
+            {
+                case Enums.CombatTier.T2:
+                    if (GameManager.Instance.IsObeliskChallenge())
+                    {
+                        isValidCombat = true;
+                        break;
+                    }
+                    break;
+                case Enums.CombatTier.T3:
+                    isValidCombat = true;
+                    break;
+                case Enums.CombatTier.T4:
+                    isValidCombat = true;
+                    break;
+                case Enums.CombatTier.T5:
+                    isValidCombat = true;
+                    break;
+                case Enums.CombatTier.T6:
+                    if (GameManager.Instance.IsObeliskChallenge())
+                    {
+                        isValidCombat = true;
+                        break;
+                    }
+                    break;
+                case Enums.CombatTier.T7:
+                    if (GameManager.Instance.IsObeliskChallenge())
+                    {
+                        isValidCombat = true;
+                        break;
+                    }
+                    break;
+            }
+
+            if (isValidCombat)
+            {
+                int i = matchManager.GetNPCAvailablePosition();
+                if (i > -1)
+                {
+                    int randomIntRange = matchManager.GetRandomIntRange(0, 100);
+                    if (randomIntRange < spawnChance)
+                    {
+                        // scarabType = 0;
+                        string str = "";
+                        if (!GameManager.Instance.IsObeliskChallenge())
+                        {
+                            if (AtOManager.Instance.GetTownTier() == 2)
+                                str = AtOManager.Instance.GetNgPlus() <= 0 ? "_b" : "_plus_b";
+                            else if (AtOManager.Instance.GetNgPlus() > 0)
+                                str = "_plus";
+                        }
+                        else if (AtOManager.Instance.GetObeliskMadness() > 8)
+                            str = combatData.CombatTier == Enums.CombatTier.T6 || combatData.CombatTier == Enums.CombatTier.T7 ? "_plus_b" : "_plus";
+                        else if (combatData.CombatTier == Enums.CombatTier.T6 || combatData.CombatTier == Enums.CombatTier.T7)
+                            str = "_b";
+                        // scarabSpawned = scarabType != 0 ? (scarabType != 1 ? (scarabType != 2 ? "scourgescarab" : "jadescarab") : "goldenscarab") : "crystalscarab";
+                        NPCData npc = Globals.Instance.GetNPC(scarabSpawned + str);
+                        if ((UnityEngine.Object)npc == (UnityEngine.Object)null)
+                        {
+                            PLog("scarabData Null for scarab => " + scarabSpawned + str);
+                        }
+                        else
+                        {
+                            CreateNPCReversePatch(npc, _position: i);
+                            Globals.Instance.WaitForSeconds(0.5f);
+                            if (scarabType != 3)
+                                TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("luckyscarab"), 1);
+                            TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("sight"), 3);
+                            if (scarabType == 1)
+                            {
+                                if (AtOManager.Instance.GetNgPlus() == 0)
+                                {
+                                    if (AtOManager.Instance.GetTownTier() == 1)
+                                        TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("zeal"), 5);
+                                    else
+                                        TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("zeal"), 7);
+                                }
+                                else if (AtOManager.Instance.GetTownTier() == 1)
+                                    TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("zeal"), 6);
+                                else
+                                    TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("zeal"), 7);
+                            }
+                            else if (scarabType == 2)
+                            {
+                                if (AtOManager.Instance.GetNgPlus() == 0)
+                                {
+                                    if (AtOManager.Instance.GetTownTier() == 1)
+                                    {
+                                        TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("evasion"), 5);
+                                        TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("buffer"), 5);
+                                    }
+                                    else
+                                    {
+                                        TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("evasion"), 7);
+                                        TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("buffer"), 7);
+                                    }
+                                }
+                                else if (AtOManager.Instance.GetTownTier() == 1)
+                                {
+                                    TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("evasion"), 6);
+                                    TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("buffer"), 6);
+                                }
+                                else
+                                {
+                                    TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("evasion"), 7);
+                                    TeamNPC[i].SetAura((Character)null, Globals.Instance.GetAuraCurseData("buffer"), 7);
+                                }
+                            }
+                            GameManager.Instance.PlayLibraryAudio("glitter", 0.25f);
+                            Globals.Instance.WaitForSeconds(0.5f);
+                        }
+                    }
+                }
+            }
+
+            Traverse.Create(matchManager).Field("scarabSpawned").SetValue(scarabSpawned);
+            Traverse.Create(matchManager).Field("combatData").SetValue(combatData);
+            Traverse.Create(matchManager).Field("teamNPC").SetValue(TeamNPC);
+            Plugin.Log.LogInfo("End Turn Prefix for Jades - END");
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,69 @@
+﻿// These are your imports, mostly you'll be needing these 5 for every plugin. Some will need more.
+
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using static Obeliskial_Essentials.Essentials;
+using System;
+
+
+// The Plugin csharp file is used to 
+
+
+// Make sure all your files have the same namespace and this namespace matches the RootNamespace in the .csproj file
+namespace MoreScarabs{
+    // These are used to create the actual plugin. If you don't need Obeliskial Essentials for your mod, 
+    // delete the BepInDependency and the associated code "RegisterMod()" below.
+
+    // If you have other dependencies, such as obeliskial content, make sure to include them here.
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.stiffmeds.obeliskialessentials")] // this is the name of the .dll in the !libs folder.
+    [BepInProcess("AcrossTheObelisk.exe")] //Don't change this
+
+    // If PluginInfo isn't working, you are either:
+    // 1. Using BepInEx v6
+    // 2. Have an issue with your csproj file (not loading the analyzer or BepInEx appropriately)
+    // 3. You have an issue with your solution file (not referencing the correct csproj file)
+
+
+    public class Plugin : BaseUnityPlugin
+    {
+        
+        // If desired, you can create configs for users by creating a ConfigEntry object here, 
+        // and then use config = Config.Bind() to set the title, default value, and description of the config.
+        // It automatically creates the appropriate configs.
+        
+        public static ConfigEntry<bool> GuaranteedSpawn { get; set; }
+        public static ConfigEntry<bool> OnlySpawnJades { get; set; }
+        public static ConfigEntry<int> PercentChanceToSpawn { get; set; }
+
+        internal int ModDate = int.Parse(DateTime.Today.ToString("yyyyMMdd"));
+        private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+
+            Log = Logger;
+            Log.LogInfo($"{PluginInfo.PLUGIN_GUID} {PluginInfo.PLUGIN_VERSION} has loaded!");
+            
+            GuaranteedSpawn = Config.Bind(new ConfigDefinition("Debug", "Guaranteed Spawn"), true, new ConfigDescription("It true, will guarantee 1 Scarab to spawn at the start of round 2."));
+            OnlySpawnJades = Config.Bind(new ConfigDefinition("Debug", "Only Spawn Jades"), true, new ConfigDescription("If true, will force all scarabs spawned to be Jade"));
+            PercentChanceToSpawn = Config.Bind(new ConfigDefinition("Debug", "Percent Chance to Spawn"), 7, new ConfigDescription("Set the percent chance for a scarab to spawn, overwritten by Guaranteed Spawn)"));
+            
+
+            // Register with Obeliskial Essentials
+            RegisterMod(
+                _name: PluginInfo.PLUGIN_NAME,
+                _author: "binbin",
+                _description: "More Scarabs",
+                _version: PluginInfo.PLUGIN_VERSION,
+                _date: ModDate,
+                _link: @"https://github.com/binbinmods/MoreScarabs"
+            );
+
+            // apply patches
+            harmony.PatchAll();
+        }
+    }
+}
