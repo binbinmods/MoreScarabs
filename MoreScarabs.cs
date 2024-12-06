462835278711034aeba11297221b7bf985092cbe
binbinmods <binbinmods@gmail.com>
2024-12-06 15:54:29 -0500

    Working reflection version

--- MoreScarabs.cs
+++ MoreScarabs.cs
@@ -7,6 +7,7 @@ using System;
 using static MoreScarabs.CustomFunctions;
 using System.Text.RegularExpressions;
 using System.Reflection;
+using UnityEngine;
 
 // Make sure your namespace is the same everywhere
 namespace MoreScarabs
@@ -17,35 +18,6 @@ namespace MoreScarabs
     public class MoreScarabPatches
     {
         public static int i = 1;
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(AtOManager), nameof(AtOManager.BeginAdventure))]
-        public static void BeginAdventurePostfix()
-        {
-            PLog("More Scarab Test");
-            // i++;
-        }
-
-        public static void CreateNPCLocal(NPCData _npcData,
-            string effectTarget = "",
-            int _position = -1,
-            bool generateFromReload = false,
-            string internalId = "",
-            CardData _cardActive = null) 
-        {
-            // PLog("Testing Reflection version before code");
-            // MethodInfo methodInfo = typeof(MatchManager).GetMethod("CreateNPC", BindingFlags.NonPublic | BindingFlags.Instance);
-            // var parameters = new object[] { _npcData, effectTarget,_position,generateFromReload,internalId,_cardActive};            
-            // methodInfo.Invoke(null,parameters);
-            // PLog("Testing Reflection version after code");
-
-            // Alternative
-            PLog("Testing Traverse Create NPC - START");
-            object[] arguments = [_npcData, effectTarget,_position,generateFromReload,internalId,_cardActive];
-            MatchManager matchManager = MatchManager.Instance;
-            Traverse.Create(matchManager).Method("CreateNPC").GetValue(arguments);
-            PLog("Testing Traverse Create NPC - END");
-
-        }
 
         // [HarmonyReversePatch]
         // [HarmonyPatch(typeof(MatchManager), "CreateNPC")]
@@ -54,32 +26,55 @@ namespace MoreScarabs
         //     int _position = -1,
         //     bool generateFromReload = false,
         //     string internalId = "",
-        //     CardData _cardActive = null) 
-        // {
+        //     CardData _cardActive = null) =>
         //     //This is intentionally a stub
-        //     PLog("Executing CreateNPCReversePatch");
-        // }
+        //     throw new NotImplementedException("Reverse patch has not been executed.");
+    
+        public static void CreateNPCLocal(NPCData _npcData,
+            string effectTarget = "",
+            int _position = -1,
+            bool generateFromReload = false,
+            string internalId = "",
+            CardData _cardActive = null)
+        {
+            MatchManager matchManager = MatchManager.Instance;
+
+            PLog("Testing Reflection version before code");
+            MethodInfo methodInfo = matchManager.GetType().GetMethod("CreateNPC", BindingFlags.NonPublic | BindingFlags.Instance);
+            var parameters = new object[] { _npcData, effectTarget,_position,generateFromReload,internalId,_cardActive};            
+            methodInfo.Invoke(matchManager,parameters);
+            PLog("Testing Reflection version after code");
+
+            // __instance.GetType().GetMethod("ActivateDeactivateButtons", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).Invoke(__instance, new object[] { index });
+
+            // Alternative
+            // PLog("Testing Traverse Create NPC - START");
+            // object[] arguments = [_npcData, effectTarget, _position, generateFromReload, internalId, _cardActive];
+            // Traverse.Create(matchManager).Method("CreateNPC").GetValue(arguments);
+            // PLog("Testing Traverse Create NPC - END");
+
+        }
 
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(MatchManager), nameof(MatchManager.EndTurn))]
         public static void EndTurnPrefix(MatchManager __instance, bool forceIt = false)
         {
-            
+
             PLog("End Turn Prefix for Jades - START");
             PLog("Jade START");
 
             MatchManager matchManager = __instance;
-            
+
 
             if (matchManager == null || matchManager.MatchIsOver)
                 return;
 
             // PLog("Match Isn't over");
-             
-            if (Traverse.Create(matchManager).Field("gameStatus").GetValue<string>() == nameof (matchManager.EndTurn) && !forceIt)
+
+            if (Traverse.Create(matchManager).Field("gameStatus").GetValue<string>() == nameof(matchManager.EndTurn) && !forceIt)
                 return;
-            
+
             // PLog("GameStatus isn't EndTurn");
 
             string scarabSpawned = Traverse.Create(matchManager).Field("scarabSpawned").GetValue<string>();
@@ -91,24 +86,24 @@ namespace MoreScarabs
 
             if (currentRound == 0 || scarabSpawned != "" || (UnityEngine.Object)combatData == (UnityEngine.Object)null || (UnityEngine.Object)Globals.Instance.GetNodeData(AtOManager.Instance.currentMapNode) == (UnityEngine.Object)null)
             {
-                if (currentRound ==0 ) PLog("Round = 0");
-                if (scarabSpawned != "" ) PLog("Scarab Spawned: " + scarabSpawned);
-                if ((UnityEngine.Object)combatData == (UnityEngine.Object)null ) PLog("null combat");
-                if ((UnityEngine.Object)Globals.Instance.GetNodeData(AtOManager.Instance.currentMapNode) == (UnityEngine.Object)null ) PLog("null mapNode");
+                if (currentRound == 0) PLog("Round = 0");
+                if (scarabSpawned != "") PLog("Scarab Spawned: " + scarabSpawned);
+                if ((UnityEngine.Object)combatData == (UnityEngine.Object)null) PLog("null combat");
+                if ((UnityEngine.Object)Globals.Instance.GetNodeData(AtOManager.Instance.currentMapNode) == (UnityEngine.Object)null) PLog("null mapNode");
                 return;
             }
-                
 
-            bool guaranteedSpawn = true; //Plugin.GuaranteedSpawn.Value;
-            bool onlyJade = true; //Plugin.OnlySpawnJades.Value
-            int percentToSwawn = 7; //Plugin.PercentChanceToSpawn.Value;
+
+            bool guaranteedSpawn = Plugin.GuaranteedSpawn.Value;
+            bool onlyJade = Plugin.OnlySpawnJades.Value;
+            int percentToSwawn = Plugin.PercentChanceToSpawn.Value;
 
             PLog("Guaranteed Spawn " + guaranteedSpawn);
             PLog("Guaranteed Jade " + onlyJade);
             int spawnChance = guaranteedSpawn ? 100 : percentToSwawn;
 
             int scarabType = onlyJade ? 2 : matchManager.GetRandomIntRange(0, 4);
-            
+
             // if scarabType==2 then Jade, if 1 then Gold, if 0 then Crystal. Else scourge
             scarabSpawned = scarabType == 2 ? "jadescarab" : scarabType == 1 ? "goldenscarab" : scarabType == 0 ? "crystalscarab" : "scourgescarab";
 
@@ -176,7 +171,7 @@ namespace MoreScarabs
                         }
                         else
                         {
-                            CreateNPCLocal(npc,_position:i);
+                            CreateNPCLocal(npc, _position: i);
                             // CreateNPCReversePatch(npc, _position: i);
                             Globals.Instance.WaitForSeconds(0.5f);
                             if (scarabType != 3)
